509bf6cd389110fdf69a67e0e9608a8f63eeb565
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-08-29 20:59:16 +0500

    movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import data from "../json/data.json";
+import React from 'react'
 
 function Movies() {
-  const [datas, setDatas] = useState([]);
-
-  useEffect(() => {
-    setDatas(data.movies);
-  }, []);
-
   return (
-    <div>
-      <p>Movies List</p>
-      {datas.map((e) => (
-        <div key={e.id}>
-          <h2>{e.title}</h2>
-          <p>Director: {e.director}</p>
-          <img src={e.poster} alt={`${e.title} poster`} />
-        </div>
-      ))}
-    </div>
-  );
+    <div>Movies</div>
+  )
 }
 
-export default Movies;
+export default Movies
