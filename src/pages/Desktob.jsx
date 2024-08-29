9e6a968ece32cbb41102e6b78a8bdc9ebc663f7e
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-08-29 21:00:21 +0500

    .

--- src/pages/Desktob.jsx
+++ src/pages/Desktob.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // Add this import
+import axios from 'axios';
 import App from '../components/Corusel';
 
 function Desktob() {
